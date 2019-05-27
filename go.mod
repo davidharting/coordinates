982ee78535bca573715d0263d58fe8adba08c36d
David Harting <davideharting@gmail.com>
2019-05-26 21:39:46 -0400

    [stub] basic endpoint with gorilla

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/davidharting/coordinates
 
 go 1.12
+
+require github.com/gorilla/mux v1.7.2
--- main.go
+++ main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 
-	"github.com/davidharting/coordinates/coordinates"
+	"github.com/gorilla/mux"
 )
 
+func getPolarCoordinates(w http.ResponseWriter, r *http.Request) {
+	v := r.URL.Query()
+	x, errX := strconv.ParseFloat(v.Get("x"), 64)
+	y, errY := strconv.ParseFloat(v.Get("y"), 64)
+
+	if errX != nil || errY != nil {
+		fmt.Fprintf(w, "Must provide query params X and Y, which must be numbers.")
+	} else {
+		fmt.Fprintf(w, "Okay then you sent us (%v, %v)", x, y)
+	}
+
+}
+
 func main() {
-	p := coordinates.CartesianPair{X: 3, Y: 21}
-	fmt.Println(p.Convert())
+	r := mux.NewRouter()
+	r.HandleFunc("/polar-coordinates", getPolarCoordinates).Methods("GET").Name("polar-coordinates")
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
